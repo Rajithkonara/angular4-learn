5f7509e7efbe8708e7a3b2a9446d9412f6108cca
Rajithkonara <rajithk@wso2telco.com>
2017-09-07 18:38:19 +0530

    recepi content added

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+
+  recipes: Recipe[] = [
+    new Recipe('A Test', 'This is a testing', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+  ];
 
   constructor() { }
 
