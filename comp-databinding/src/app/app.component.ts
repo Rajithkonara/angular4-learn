d7dce8cd490df098da2663c47d32b49a5ae4691a
Rajithkonara <rajithkonara@gmail.com>
2017-09-09 23:03:53 +0530

    binding custom events input output

--- comp-databinding/src/app/app.component.ts
+++ comp-databinding/src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
 
   serverElements = [{ type:'server', name: 'test', content: 'Just A Test' }];
   
+  onServerAdded (serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded (blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  } 
+
 }
