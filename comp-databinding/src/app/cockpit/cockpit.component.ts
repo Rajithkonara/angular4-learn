3b715a6c8d8a4e2084cdfeb422309cae5f78c2a9
Rajithkonara <rajithkonara@gmail.com>
2017-09-10 10:41:46 +0530

    viewChild

--- comp-databinding/src/app/cockpit/cockpit.component.ts
+++ comp-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,6 +10,10 @@ export class CockpitComponent implements OnInit {
   @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
+ 
+  // arg selector
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
@@ -18,7 +22,7 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) { 
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
