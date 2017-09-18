4262afe92c92cf638f8ae17ce786825ec047b7ae
Rajithkonara <rajithk@wso2telco.com>
2017-09-18 08:07:30 +0530

    cross component comm

--- shopping/src/app/recipes/recipe.service.ts
+++ shopping/src/app/recipes/recipe.service.ts
@@ -1,8 +1,11 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+   
+  recipeSelected = new EventEmitter<Recipe>();   
   
- private recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe('A Test', 'This is a testing', 
     'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
     new Recipe('Butter Cuttle Fish', 'Righ Now ............',
