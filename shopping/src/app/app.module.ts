4f09d8f368391973acbf4798623ff53fd8be9764
Rajithkonara <rajithk@wso2telco.com>
2017-09-18 08:56:08 +0530

    shopping list service

--- shopping/src/app/app.module.ts
+++ shopping/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-i
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropDownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { DropDownDirective } from './shared/dropdown.directive';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- shopping/src/app/shopping-list/shopping-list.service.ts
+++ shopping/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
+ 
+   private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Guwawa', 6),
+      ];
+
+    getIngredients () {
+        return this.ingredients.slice();
+    }
+
+    addIngredients (ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
     
 }
