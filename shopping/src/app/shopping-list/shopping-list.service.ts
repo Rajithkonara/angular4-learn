d203d044cb17637abbadc43c5593bdf9bd792fc9
Rajithkonara <rajithk@wso2telco.com>
2017-09-18 09:09:30 +0530

    service for push

--- shopping/src/app/shopping-list/shopping-list.service.ts
+++ shopping/src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,10 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
- 
+   
+    ingredientsChange = new EventEmitter<Ingredient[]>();
+
    private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Guwawa', 6),
@@ -13,6 +16,7 @@ export class ShoppingListService {
 
     addIngredients (ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChange.emit(this.ingredients.slice());
     }
     
 }
