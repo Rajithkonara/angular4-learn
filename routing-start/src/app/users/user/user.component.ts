948b36cd0d94cf0ca600ba622d4fe23a5018e0c5
Rajithkonara <rajithk@wso2telco.com>
2017-09-25 12:16:56 +0530

    unsubscribe observable

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+ 
+  user: { id: number, name: string };
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,13 +20,18 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     // params are observables - asynchronous
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+
+  // angular handles this
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
