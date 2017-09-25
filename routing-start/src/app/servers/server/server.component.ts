4bab875c957882fe32e7895dad068a56ef638d4b
Rajithkonara <rajithk@wso2telco.com>
2017-09-25 14:06:55 +0530

    clean up

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,7 +16,15 @@ export class ServerComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+      // + converts string to a number
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params 
+      .subscribe(
+        (params: Params) => {
+           this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
   onReload () {
