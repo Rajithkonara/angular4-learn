f75193df2f901a28dccc45f5b9a120d1ad093c7a
Rajithkonara <rajithk@wso2telco.com>
2017-10-12 10:09:45 +0530

    fake auth service 👮

--- routing-start/src/app/auth.service.ts
+++ routing-start/src/app/auth.service.ts
@@ -6,7 +6,7 @@ export class AuthService {
             (resolve, reject) => {
                 setTimeout(() => {
                     resolve(this.loggedIn)
-                }, 8000);
+                }, 3000);
         }
       );
       return promise;
