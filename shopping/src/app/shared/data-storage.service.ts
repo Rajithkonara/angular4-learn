866fb3a4081e2c046023cc364e468128ab177491
Rajithkonara <rajithkonara@gmail.com>
2017-12-28 14:47:17 +0530

    empty array fix

--- shopping/src/app/shared/data-storage.service.ts
+++ shopping/src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'; 
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
@@ -14,9 +14,20 @@ export class DataStorageService {
 
     getRecipes() {
         return this.http.get('url').
-            subscribe(
+            map(
                 (response: Response) => {
                     const recipes: Recipe[] = response.json();
+                    for (let recipe of recipes) {
+                        if (!recipe['ingredients']) {
+                            console.log(recipe);
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            )
+            .subscribe(
+                (recipes: Recipe[]) => {
                     this.recipeService.setRecipes(recipes);
                 }
             ); 
